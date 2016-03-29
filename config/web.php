b4114c35722daf0737c79d8170583872842d980b
chenyi <chenyi@huogou.com>
2016-03-29 11:59:51 +0800

    yii2

--- config/web.php
+++ config/web.php
@@ -6,6 +6,14 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'modules' => [
+        'client' => [
+            'class' => 'app\modules\client\Module',
+        ],
+        'server' => [
+            'class' => 'app\modules\server\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
